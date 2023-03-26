2e576d017c054f9852b5139c07661265e3a8c337
soaresWT <victorsuares2014@gmail.com>
2023-03-26 15:53:01 -0300

    testando a coneccao

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Cartao from './components/Cartao/Cartao';
+
+function App() {
+  return (
+    <>
+      <Cartao/>
+      <Cartao/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Cartao/Cartao.jsx
@@ -0,0 +1,13 @@
+import './style.css'
+
+const Cartao = () => {
+    return(
+        <div className="teste">
+            <h1>grgg</h1>
+            <h2>egwge</h2>
+            <span>gegeg</span>
+        </div>
+    );
+}
+
+export default Cartao;
